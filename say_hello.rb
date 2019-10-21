d3ee37331e00fec3bc683b839082603d626cbc92
Kate Summerill <kaitlynsummerill@gmail.com>
2019-10-21 03:29:16 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 
-def say_hello(name = "Ruby Programmer")
-  puts = "Hello #{}!"
+def say_hello(name = "Ruby programmer")
+  puts "Hello #{}!"
 end
 
 say_hello("Gabriela")
